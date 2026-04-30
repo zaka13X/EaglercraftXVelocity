d0cd0bbb17af1a9e2b31b9ff348644822fef5071
zaka13 <105675209+zaka13X@users.noreply.github.com>
2026-04-30 15:42:32 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-
+# Create default config if it doesn't exist
 if [ ! -f velocity.toml ]; then
     java -jar velocity.jar
 
@@ -14,7 +14,9 @@ sed -i 's/player-info-forwarding-mode = ".*"/player-info-forwarding-mode = "lega
 if [ -n "$BACKEND_NAME" ] && [ -n "$BACKEND_ADDRESS" ]; then
     echo "[servers]" > servers.toml
     echo "$BACKEND_NAME = \"$BACKEND_ADDRESS\"" >> servers.toml
-    
+   
 fi
 
-exec java -Xms512M -Xmx512M -jar velocity.jar
+exec java -Xms350M -Xmx350M -jar velocity.jar
+
+    
